9e03589cad0c61c43f5b4729ad77f7abbce60670
Jeet Paul <jeetpaul2222@gmail.com>
2025-12-23 20:56:51 +0530

    feat: add Card and Container components to manage children props layout

--- src/components/ChildrenProps.jsx
+++ src/components/ChildrenProps.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+const Card = ({ children, title, color = "blue" }) => {
+  const colorClasses = {
+    blue: "border-blue-500 bg-blue-500",
+    green: "border-green-500 bg-green-500",
+    purple: "border-purple-500 bg-purple-500",
+    red: "border-red-500 bg-red-500",
+  };
+  return (
+    <div
+      className={`${colorClasses[color]} border-l-4 p-6 rounded-lg shadow-md`}
+    >
+      {title && (
+        <h2 className="text-xl font-bold mb-3 text-gray-600">{title}</h2>
+      )}
+      <div className="text-gray-700">{children}</div>
+    </div>
+  );
+};
+
+const Container = ({ children, layout = "vertical" }) => {
+  const layoutClasses = {
+    vertical: "flex flex-col space-y-4",
+    horizontal: "flex flex-row  flex-wrap gap-4",
+    grid: "grid grid-cols-1 md:grid-cols-2 gap-4",
+  };
+  return <div className={`${layoutClasses[layout]}`}>{children}</div>;
+};
 
 const ChildrenProps = () => {
-  return (
-    <div>ChildrenProps</div>
-  )
-}
+  return <div>ChildrenProps</div>;
+};
 
-export default ChildrenProps
+export default ChildrenProps;
