948668caa4dcb93ed116eefe8511b139a9ace10f
Jeet Paul <jeetpaul2222@gmail.com>
2025-12-26 19:19:25 +0530

    feat: implement ThemeProvider with context for theme management and toggle functionality

--- src/components/ThemeToggler.jsx
+++ src/components/ThemeToggler.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Children, createContext, useState } from "react";
+
+// Create a Context for the theme
+const ThemeContext = createContext(); //step-1
+// Theme Provider component
+export const ThemeProvider = ({ Children }) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  const value = {
+    theme,
+    toggleTheme,
+    isDark: theme === "dark",
+  };
+
+  return <ThemeContext.Provider value={value}>{Children}</ThemeContext.Provider>;
+};
 
 const ThemeToggler = () => {
-  return (
-    <div>ThemeToggler</div>
-  )
-}
+  return <div>ThemeToggler</div>;
+};
 
-export default ThemeToggler
+export default ThemeToggler;
